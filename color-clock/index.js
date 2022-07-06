f3ed78ad481f6fbc7e2bc7a9c35715ada83cebbc
Naomi Cole <naomisarahcole@gmail.com>
2022-07-06 17:42:24 +1000

    Completed inclass'

--- color-clock/index.js
+++ color-clock/index.js
@@ -4,6 +4,7 @@
 function displayTime() {
   let clock = document.querySelector('#clock')
   clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
+ 
   setInterval(function() {
       clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
       clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
@@ -13,4 +14,4 @@ function displayTime() {
 
 }
 
-window.onload = displayTime()
+window.onload = displayTime();
